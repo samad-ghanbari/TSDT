e57632ff812530075edc8d50fad903c8ad655321
Samad Ghanbari <ghanbari.samad@gmail.com>
2023-11-18 00:14:07 +0330

    backend - postgres connection

--- backend/controllers/baseController.js
+++ backend/controllers/baseController.js
@@ -1,9 +1,29 @@
+const { Sequelize } = require("sequelize");
+
 class baseController {
   constructor() {}
 
   // ************************ Login
-  static login(req, res) {
-    return res.json({ data: "login page" });
+  static async login(req, res) {
+    var db_status = "";
+
+    const sequelize = new Sequelize(
+      "tsdt",
+      process.env.DB_USER,
+      process.env.DB_PASS,
+      {
+        host: "localhost",
+        dialect: "postgres",
+      }
+    );
+    try {
+      await sequelize.authenticate();
+      db_status = "Connection has been established successfully.";
+    } catch (error) {
+      db_status = "Unable to connect to the database: " + error;
+    }
+
+    return res.json({ db: db_status });
   }
 
   // ************************
